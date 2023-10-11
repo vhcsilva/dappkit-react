42fe8aa2548e7dfbd38010a31ed1aeb134ca3049
MoshMage <moshmage@gmail.com>
2023-10-11 17:19:08 +0100

    fix hooks on metamask-card and introduce gnosis-safe

--- src/components/connector-card/gnosis-safe-card/gnosis-safe-card.tsx
+++ src/components/connector-card/gnosis-safe-card/gnosis-safe-card.tsx
@@ -1,18 +1,18 @@
-import {CustomConnectorCardProps} from "../../../types/connector-card";
-import {useConnectorHooks} from "../../../hoox/use-connector-hooks";
-import {useConnectEagerly} from "../../../hoox/use-connect-eagerly";
 import ConnectorCard from "../connector-card";
 import {Chains} from "../../../connectors/chains";
-import {metamaskWallet, hooks} from "../../../connectors/metamask-wallet";
+import {useConnectorHooks} from "../../../hoox/use-connector-hooks";
+import {useConnectEagerly} from "../../../hoox/use-connect-eagerly";
+import {CustomConnectorCardProps} from "../../../types/connector-card";
 import {useDappkitConnectionInfo} from "../../../hoox/use-dappkit";
+import {gnosisSafe, hooks} from "../../../connectors/gnosis-safe";
 
-export default function MetamaskCard({onConnectorConnect, onConnectorDisconnect}: CustomConnectorCardProps) {
+export default function GnosisSafeCard({onConnectorConnect, onConnectorDisconnect}: CustomConnectorCardProps) {
   const {isActivating, error, setError} = useConnectorHooks(hooks);
   const {chainId, connected} = useDappkitConnectionInfo();
 
-  useConnectEagerly(metamaskWallet);
+  useConnectEagerly(gnosisSafe);
 
-  return <ConnectorCard connector={metamaskWallet}
+  return <ConnectorCard connector={gnosisSafe}
                         chainIds={Chains}
                         activeChainId={chainId || 0}
                         isActivating={isActivating}
