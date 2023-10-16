7ac09a2cbdb87e373fb748aa66587007929e80d2
MoshMage <moshmage@gmail.com>
2023-10-16 12:12:08 +0100

    remove repeated typins

--- src/lib/types/connector-button.ts
+++ src/lib/types/connector-button.ts
@@ -1,18 +1,14 @@
-import type {provider as Provider} from "web3-core";
 import {Connector} from "@web3-react/types";
 import {ReactNode} from "react";
-import {ModalModes} from "./wallet-selector";
+import {CustomConnectorButtonProps} from "./custom-connector-button";
 
 export type Props = {
   connector: Connector;
-  onConnectorConnect(p: Provider): void
-  onConnectorDisconnect(): void
   setError: (e?: Error) => void;
   error: Error|undefined;
   isActive: boolean;
   activeChainId: number;
   logo?: ReactNode;
-  variant: ModalModes;
 };
 
-export type ConnectorButtonProps = Props;
+export type ConnectorButtonProps = Props & CustomConnectorButtonProps;
