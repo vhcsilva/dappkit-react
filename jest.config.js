43bdac8ef249090d7f0c9998170009809a5aece0
MoshMage <moshmage@gmail.com>
2023-10-25 15:21:20 +0100

    add jest config to package

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-  collectCoverage: true,
-  coverageProvider: 'v8',
-  testMatch: [ "**/?(*.)+(spec|test).[jt]s?(x)" ],
-  watch: false
-};
